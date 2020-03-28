f07428d30f9a33695b711968e3555eb6f8b50b4b
unknown <calebeguimaraes10@gmail.com>
2020-03-28 20:16:04 -0300

    integration test and unit test

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,4 +10,4 @@ app.use(express.json())
 app.use(routes)
 app.use(errors())
 
-app.listen(3333, console.log('API Be the Hero 1.0.0: PORT 3333'))
+module.exports = app
---
+++ backend/src/server.js
@@ -0,0 +1,3 @@
+const app = require('./app')
+
+app.listen(3333, console.log('API Be the Hero 1.0.0: PORT 3333'))
