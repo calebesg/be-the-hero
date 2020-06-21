1835591e12d89fe5488622b22380e4112441e129
CalebeSG <calebeguimaraes10@gmail.com>
2020-06-20 21:56:10 -0300

    Config status bar

--- mobile/App.js
+++ mobile/App.js
@@ -2,9 +2,19 @@ import 'intl'
 import 'intl/locale-data/jsonp/pt-BR'
 
 import React from 'react'
+import { StatusBar } from 'react-native'
 
 import Routes from './src/routes'
 
-const App = () => <Routes />
-
-export default App
+export default function App() {
+  return (
+    <>
+      <StatusBar 
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Routes />
+    </>
+  )
+} 
